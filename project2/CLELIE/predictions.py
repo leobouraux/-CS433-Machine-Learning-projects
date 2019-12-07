b555759395ae8748818eeaaf347f35eb49579d0b
Léopold Bouraux <leopold.bouraux@epfl.ch>
2019-12-07 17:06:49 +0100

    Megre

--- project2/CLELIE/predictions.py
+++ project2/CLELIE/predictions.py
@@ -4,22 +4,8 @@ import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 from PIL import Image
 
-from im_processing import *
-#from model_example import *
-from helpers_model import *
-
-NUM_CHANNELS    = 3  # RGB images
-PIXEL_DEPTH     = 255
-NUM_LABELS      = 2
-TRAINING_SIZE   = 20
-VALIDATION_SIZE = 5  # Size of the validation set.
-SEED            = 66478  # Set to None for random seed.
-BATCH_SIZE      = 16  # 64
-NUM_EPOCHS      = 100
-RESTORE_MODEL   = False  # If True, restore existing model instead of training a new one
-RECORDING_STEP  = 0
-FG_THRESH       = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
-IMG_PATCH_SIZE  = 16 # IMG_PATCH_SIZE should be a multiple of 4, image size should be an integer multiple of this number
+from constant_values import *
+from img_preprocessing import img_crop
 
 
 def error_rate(predictions, labels):
@@ -87,5 +73,3 @@ def get_prediction_with_overlay(filename, image_idx, model):
     oimg = make_img_overlay(img, img_prediction)
 
     return oimg
-
-
