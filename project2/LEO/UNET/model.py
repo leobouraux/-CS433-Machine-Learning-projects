51811b5f6ee76631a5be1cf56d47a582467ac09d
Léopold Bouraux <leopold.bouraux@epfl.ch>
2019-12-09 14:22:30 +0100

    Try to delete h5 file

--- project2/LEO/UNET/model.py
+++ project2/LEO/UNET/model.py
@@ -1,11 +1,20 @@
+import numpy as np 
+import os
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
 from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as keras
 
 
-def model_UNET(input_size = (16, 16 ,3)):
-    inputs = Input(input_size)
+def unet(input_size = (256,256,3), pretrained_weights=None):
+    print('avant')
+    inputs = Input(shape=(256,256,3))
+    print('apres')
+
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
@@ -47,7 +56,14 @@ def model_UNET(input_size = (16, 16 ,3)):
     conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
     model = Model(input = inputs, output = conv10)
+
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
     
+    #model.summary()
+
+    if(pretrained_weights):
+    	model.load_weights(pretrained_weights)
+
     return model
 
 
